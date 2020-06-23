00aef41db5a9616616b94a9457666bd7848162e8
Calvin Smith <cjsmith@cs.wisc.edu>
2020-06-22 21:35:54 -0500

    data: added first ten presidents for two diff tasks

--- experiments/experiments.mk
+++ experiments/experiments.mk
@@ -1,1 +1,6 @@
+# experiments sub-makefile
+#
+# contains includes for each experiment's pipeline
+
+# EXPERIMENT 1 - test of tool, using Lincoln's place of birth as a target
 include experiments/lincoln-test/pipeline.mk
